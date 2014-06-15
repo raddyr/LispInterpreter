8c461fddecd7ed3022e3d185904425fe1fe58f9c
Lukasz Szymanski <szyman@szyman-K73SV.(none)>
2014-06-15 23:32:38 +0200

    interpreter-style

--- main.py
+++ main.py
@@ -6,17 +6,36 @@ from Interpreter import Interpreter
 
 
 if __name__ == '__main__':
-    try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
-        file = open(filename, "r")
-    except IOError:
-        print("Cannot open {0} file".format(filename))
-        sys.exit(0)
+    if len(sys.argv) > 1:
+        try:
+            filename = sys.argv[1]
+            file = open(filename, "r")
+        except IOError:
+            print("Cannot open {0} file".format(filename))
+            sys.exit(0)
 
-    LISPparser = LISPparser()
-    parser = yacc.yacc(module=LISPparser)
-    text = file.read()
+        LISPparser = LISPparser()
+        parser = yacc.yacc(module=LISPparser)
 
-    ast = parser.parse(text, lexer=LISPparser.scanner)
-    
-    ast.accept2(Interpreter())
+        text = file.read()
+        ast = parser.parse(text, lexer=LISPparser.scanner)
+        ast.accept2(Interpreter())
+    else:
+        LISPparser = LISPparser()
+        parser = yacc.yacc(module=LISPparser)
+
+        print "Type \"exit\" or empty line to quit"
+
+        lineNumber = 1
+        print "["+str(lineNumber)+"]>>",
+        line = sys.stdin.readline()
+        lineNumber += 1
+
+        while line:
+            if line.rstrip("\n") == "exit":
+                break
+            ast = parser.parse(line, lexer=LISPparser.scanner)
+            ast.accept2(Interpreter())
+            print "["+str(lineNumber)+"]>>",
+            line = sys.stdin.readline()
+            lineNumber += 1
--- makefile
+++ makefile
@@ -5,5 +5,10 @@ all : run clean
 run:
 	python main.py
 
+rune: example clean
+
+example:
+	python main.py example.txt
+
 clean: 
 	rm -f *.pyc parser.out parsetab.py *~
